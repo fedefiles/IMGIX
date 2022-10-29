4efc95c7d07b9e30980a703981c5fa8b4eabe3f4
fedefiles <intimarca@gmail.com>
2022-10-29 20:15:34 -0300

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
-import { Container, Row, Navbar, Button, Text, Card, Radio, Spacer } from "@nextui-org/react";
+import { Container, Row, Navbar, Button, Text, Card, Radio, Spacer, Col } from "@nextui-org/react";
 import { Link} from 'react-router-dom';
 import Main from './Main';
+import Rotate from './Rotate';
 export default function App() {
   return (
     <Container>
-    <Card css={{ $$cardColor: '$colors$gradient' }}>
+    <Card css={{ $$cardColor: '$colors$neutral' }}>
       <Card.Body>
         <Row justify="center" align="center" display="flex">
+            <Col>
+            <Button> onPress={() => Rotate }</Button>
+            </Col>          
+            <Col>
             <Button> <Link to='/'>Home</Link></Button>
+            </Col>
+            <Col>
             <Button> <Link to='/rotate'>Rotate</Link></Button>
+            </Col>
+            <Col>
             <Button> <Link to='/adjust'>Adjust</Link></Button>
+            </Col>
         </Row>
       </Card.Body>
     </Card>
