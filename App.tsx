dd0905c00dffe157639ea27e889da65604978de5
fedefiles <intimarca@gmail.com>
2022-10-29 19:03:29 -0300

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
+import { Container, Row, Navbar, Button, Text, Card, Radio } from "@nextui-org/react";
 import { Link} from 'react-router-dom';
 import Main from './Main';
 export default function App() {
   return (
-    <>  
-      <div>
-        <ul>
+    <Container>
+    <Card css={{ $$cardColor: '$colors$primary' }}>
+      <Card.Body>
+        <Row justify="center" align="center">
+          <Text h6 size={15} color="white" css={{ m: 0 }}>
+          <ul>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/rotate'>Rotate</Link></li>
           <li><Link to='/adjust'>Adjust</Link></li>
         </ul>
-        <hr />
-        <Main />       
-      </div>   
-    </>
+        
+              
+          </Text>
+        </Row>
+      </Card.Body>
+    </Card>
+    <Main /> 
+  </Container>
+  
   )
-}
+};
