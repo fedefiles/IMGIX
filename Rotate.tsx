1d57242554b2537f73b5df306ff368807d348f13
fedefiles <intimarca@gmail.com>
2022-10-30 06:54:11 -0300

    1 file modified

--- Rotate.tsx
+++ Rotate.tsx
@@ -70,35 +70,7 @@ const Rotate = () => {
     
   return (
     <div>
-      <Card css={{ w: "100%", h: "500px" }}>
-   
-   <Card.Body css={{ p: 0 }}>
-     <Card.Image
-       src={urls[index]}
-       objectFit="cover"
-       width="100"
-       height="100%"
-       alt="Relaxing app background"
-       
-     />
-   </Card.Body>
-   <Card.Footer
-     isBlurred
-     css={{
-       position: "absolute",
-       bgBlur: "#0f111466",
-       borderTop: "$borderWeights$light solid $gray800",
-       bottom: 0,
-       zIndex: 1,
-     }}
-   >
-     <Row justify="center" align="center">
-       
      
-         
-     </Row>
-   </Card.Footer>
- </Card>
  <Imgix
         src={urls[index]}
         sizes="400px"
