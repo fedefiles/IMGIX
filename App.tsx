6a12d819dab31ce8cd355a60be1bc22f4b3d6cdf
fedefiles <intimarca@gmail.com>
2022-10-31 07:46:55 -0300

    1 file modified

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import Main from './Main';
 import Rotate from './Rotate';
 export default function App() {
   return (
-    <Container>
+    <Container fluid>
       <Row justify="center" align="center">
       <Card css={{ $$cardColor: '$colors$primary' }}>
     <Card.Body>
