827faf308993dea71ffe055c466a106b8f32a596
fedefiles <intimarca@gmail.com>
2022-10-31 02:51:27 -0300

    1 file modified

--- Home.tsx
+++ Home.tsx
@@ -24,6 +24,36 @@ const Home = () => {
     <Card0>
 
     </Card0>
+    <Spacer x={2} />
+    <Card css={{ w: "100%", h: "500px" }}>
+   
+   <Card.Body css={{ p: 0 }}>
+     <Card.Image
+       src="https://assets.imgix.net/gif-examples/imgix-logo-ani.gif"
+       objectFit="cover"
+       width="100"
+       height="100%"
+       alt="Relaxing app background"
+       
+     />
+   </Card.Body>
+   <Card.Footer
+     isBlurred
+     css={{
+       position: "absolute",
+       bgBlur: "#0f111466",
+       borderTop: "$borderWeights$light solid $gray800",
+       bottom: 0,
+       zIndex: 1,
+     }}
+   >
+     <Row justify="center" align="center">
+       
+     
+         
+     </Row>
+   </Card.Footer>
+ </Card>
      </div>
   )
 };
