8c9b1ea3f6a74a144e5741502866ffa1154c7cd7
fedefiles <intimarca@gmail.com>
2022-10-31 01:22:02 -0300

    1 file modified

--- Home.tsx
+++ Home.tsx
@@ -5,43 +5,7 @@ import {Card0} from "./Card0"
 
 
 
-let urls: string[] = ["https://assets.imgix.net/unsplash/alarmclock.jpg",
-"https://assets.imgix.net/unsplash/bear.jpg",
-"https://assets.imgix.net/unsplash/bridge.jpg",
-"https://assets.imgix.net/unsplash/citystreet.jpg",
-"https://assets.imgix.net/unsplash/coffee.JPG",
-"https://assets.imgix.net/unsplash/coyote.jpg",
-"https://assets.imgix.net/unsplash/goldengate.jpg",
-"https://assets.imgix.net/unsplash/macaque.jpg",
-"https://assets.imgix.net/unsplash/motorbike.jpg",
-"https://assets.imgix.net/unsplash/mountains.jpg",
-"https://assets.imgix.net/unsplash/paperlamp.jpg",
-"https://assets.imgix.net/unsplash/pineneedles.jpg",
-"https://assets.imgix.net/unsplash/raspberries.jpg",
-"https://assets.imgix.net/unsplash/transport.jpg",
-"https://assets.imgix.net/unsplash/turntable.jpg",
-"https://assets.imgix.net/unsplash/typewriter.jpg",
-"https://assets.imgix.net/unsplash/umbrella.jpg",
-"https://assets.imgix.net/unsplash/vintagecameras.jpg",
-"https://assets.imgix.net/unsplash/windows.jpg",
-"https://assets.imgix.net/unsplash/womansitting.jpg",
-"https://assets.imgix.net/examples/balloons.jpg",
-"https://assets.imgix.net/examples/blueberries.jpg",
-"https://assets.imgix.net/examples/bluehat.jpg",
-"https://assets.imgix.net/examples/butterfly.jpg",
-"https://assets.imgix.net/examples/espresso.jpg",
-"https://assets.imgix.net/examples/jellyfish.jpg",
-"https://assets.imgix.net/examples/kingfisher.jpg",
-"https://assets.imgix.net/examples/lorie.png",
-"https://assets.imgix.net/examples/mountain.jpg",
-"https://assets.imgix.net/examples/octopus.jpg",
-"https://assets.imgix.net/examples/puffins.jpg",
-"https://assets.imgix.net/examples/redleaf.jpg",
-"https://assets.imgix.net/examples/snake.jpg",
-"https://assets.imgix.net/examples/treefrog.jpg",
-"https://assets.imgix.net/examples/vista.png",
-"https://assets.imgix.net/examples/womanlandscape.jpg",
-"https://assets.imgix.net/gif-examples/imgix-logo-ani.gif"];
+
 
 const Home = () => {
  
@@ -60,7 +24,7 @@ const Home = () => {
 
 
   <Card0>
-    
+
     </Card0>
    
      
