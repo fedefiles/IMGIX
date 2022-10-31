c385213e8f1457b02a867dd11f47ef5a9a089364
fedefiles <intimarca@gmail.com>
2022-10-31 08:46:53 -0300

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
-import { Container, Row, Navbar, Button, Text, Card, Radio, Spacer, Col } from "@nextui-org/react";
+import { Container, Row, Grid, Button, Text, Card, Radio, Spacer, Col } from "@nextui-org/react";
 import { Link} from 'react-router-dom';
 import Main from './Main';
 import Rotate from './Rotate';
 export default function App() {
   return (
+    
     <Container  >
-      <Row justify="center" align="center">
-      <Card css={{ $$cardColor: '$colors$primary' }}>
-    <Card.Body>
-      <Row  align="center">
-     
-            <Col>
-           
-            <Button 
+      
+          <Grid.Container gap={6}>
+        <Grid>
+        <Button 
   Medium 
   as="a"
   rel="noopener noreferrer"
@@ -41,9 +38,9 @@ export default function App() {
   >
     Home
 </Button>
-</Col>
-            <Col>
-            <Button 
+        </Grid>
+        <Grid>
+        <Button 
   Medium 
   as="a"
   rel="noopener noreferrer"
@@ -71,10 +68,9 @@ export default function App() {
   >
    Rotate
 </Button>
-          
-            </Col>
-            <Col>
-            <Button 
+        </Grid>
+        <Grid>
+        <Button 
   Medium 
   as="a"
   rel="noopener noreferrer"
@@ -102,16 +98,17 @@ export default function App() {
   >
    Adjust
 </Button>
-            </Col>
-      </Row>
-    </Card.Body>
-    </Card>
-     
-      </Row>
+</Grid>
+   </Grid.Container>  
+    
+  
+  
   
     <Spacer y={1} />
     <Main /> 
+
   </Container>
   
-  )
+  
+)
 };
